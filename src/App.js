159f9915c3ba94f4e08fcbdefd3c7921e876cf79
Binura99 <binurayasith12@gmail.com>
2024-06-13 15:32:30 +0530

    Add red color

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="Decryptogen Tech Logo" />
-        <p>Welcome to the Devops Workshop 2024</p>
+        <p>Welcome to the <span style={{color: "red"}}>Devops Workshop</span> 2024</p>
       </header>
     </div>
   );
